02ad152e725030261274b94406e79d9dbcf6287c
DabeenKim <iok483@naver.com>
2023-06-26 16:02:28 +0900

    feat: about 퍼블리싱

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,20 @@
-// 스크롤 좌표
-window.addEventListener("scroll", () => {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
+$(function () {
+  // 스크롤 좌표
+  window.addEventListener("scroll", () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
 
-  document.querySelector(".scroll").innerText = Math.round(scrollTop);
-});
+    document.querySelector(".scroll").innerText = Math.round(scrollTop);
+  });
 
-// 스크롤러
-let s = skrollr.init({
-  smoothScrolling: true,
-  smoothScrollingDuration: 700,
+  // 스크롤러
+  let s = skrollr.init({
+    forceHeight: false,
+    smoothScrolling: true,
+    smoothScrollingDuration: 400,
+  });
+
+    $(".menu-btn").on("click",function(){
+      $(this).toggleClass("active");
+    });
+
 });
