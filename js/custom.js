9c71e68fad410bb5d421521d8d9431416555cc00
dabin483 <81741760+dakong8@users.noreply.github.com>
2023-07-05 21:33:42 +0900

    fix: about 퍼블리싱 수정

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,34 @@
 $(function () {
-  // 스크롤 좌표
+  // Scroll coord
   window.addEventListener("scroll", () => {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
 
     document.querySelector(".scroll").innerText = Math.round(scrollTop);
   });
 
-  // 스크롤러
+  // Skrollr
   let s = skrollr.init({
     forceHeight: false,
-    smoothScrolling: true,
-    smoothScrollingDuration: 400,
+    smoothScrolling: false,
+    // smoothScrollingDuration: 200
   });
 
-    $(".menu-btn").on("click",function(){
-      $(this).toggleClass("active");
-    });
+  // Menu
+  $(".menu-btn").on("click",function(){
+    $(this).toggleClass("active");
+  });
 
+  // About star
+  function addStar() {
+    const star = document.createElement("span");
+    star.className = "star";
+    star.style.setProperty("--size", Math.random() * 10 + 3 + "vmin");
+    star.style.left = Math.floor(Math.random() * 100) + "%";
+    star.style.top = Math.floor(Math.random() * 100) + "%";
+    
+    document.querySelector(".main .contents").appendChild(star);
+  }
+
+  setInterval(addStar, 40);
+  
 });
