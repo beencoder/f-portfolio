27cfc7ca19ac15bf62e20fcc09f21ea6dd813dd3
dabin483 <81741760+dakong8@users.noreply.github.com>
2023-07-18 22:43:12 +0900

    feat: project 퍼블리싱 중간 작업

--- js/custom.js
+++ js/custom.js
@@ -26,9 +26,41 @@ $(function () {
     star.style.left = Math.floor(Math.random() * 100) + "%";
     star.style.top = Math.floor(Math.random() * 100) + "%";
     
-    document.querySelector(".main .contents").appendChild(star);
+    document.querySelector(".about .contents").appendChild(star);
+  }
+  // setInterval(addStar, 40);
+
+  const temp = document.querySelectorAll(".pj-img img");
+
+  temp.forEach((item, i) => {
+    item.addEventListener("click", () => {
+      item.src = `images/project-thumb${i + 1}.png`;
+    });
+  });
+
+});
+
+// Add Paint Element
+function addPaint() {
+  const contents = document.querySelectorAll(".about .cont1, .about .cont2, .about .cont3");
+  const titPages = document.querySelectorAll(".paint-bg");
+
+  for (let i = 0; i < contents.length; i++) {
+    for (let s = 0; s < 8; s++) {
+      const paint = document.createElement("span");
+      paint.className = "paint";
+      document.querySelector(`.${contents[i].className}`).appendChild(paint);
+    }
   }
 
-  // setInterval(addStar, 40);
+  for (let i = 0; i < titPages.length; i++) {
+    for (let s = 0; s < 17; s++) {
+      const paint = document.createElement("span");
+      paint.className = "paint";
+      paint.style.setProperty("--size", Math.random() * 50 + "vmin");
+      document.querySelector(`#${titPages[i].offsetParent.id} .${titPages[i].className}`).appendChild(paint);
+    }
+  }
   
-});
+}
+addPaint();
